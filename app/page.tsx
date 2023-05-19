a65c2d85b746ded7b0a3f9924b86e0786259409f
Klemen Pavlovič <klemenpavlovic@gmail.com>
2023-05-19 12:59:08 +0200

    Add example protected route

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
+
 export default function Home() {
+  const session = getServerSession();
+
+  // You can use redirect in combination with session data to protect your route and redirect any users that are not logged in
+  // if (!session) {
+  //   console.log(session);
+  //   redirect("/api/auth/signin");
+  //   // return <p>You must be signed in...</p>
+  // }
+
   return (
     <div>
       <h1>Welcome to NextSpace!</h1>

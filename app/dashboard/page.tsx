9cf030a98da7300e5916b9522a9b3750adf2c3f5
Klemen Pavlovič <klemenpavlovic@gmail.com>
2023-05-22 13:13:24 +0200

    Add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import { getServerSession } from "next-auth";
+import { prisma } from "@/lib/prisma";
+import { ProfileForm } from "./ProfileForm";
+import { redirect } from "next/navigation";
+import { SignOutButton } from "@/components/buttons";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export default async function Dashboard() {
+  // Get server session for current user, so non-users can't access this route
+  // You need to pass authOptions in this version of next-auth here for it to work properly
+  const session = await getServerSession(authOptions);
+
+  // If session is null redirect the user
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  // Find the current user in database based on the email
+  const currentUserEmail = session?.user?.email!;
+  const user = await prisma.user.findUnique({
+    where: {
+      email: currentUserEmail,
+    },
+  });
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <SignOutButton />
+      <ProfileForm user={user} />
+    </>
+  );
+}
